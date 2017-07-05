c063ecc3e8ffde4254dd38ba9cbe6d341a5ba63c
Mikhail Posypkin <mposypkin@gmail.com>
2017-07-05 16:50:57 +0300

    [upd] move semantics

--- box/box.hpp
+++ box/box.hpp
@@ -1,35 +1,89 @@
 #ifndef __BOX_HPP__
 #define __BOX_HPP__
 
-#include <common/smartptr.hpp>
+#include <cstring>
+#include <utility>
 /**
-  * Class for handling boxes
-  */
+ * Class for handling boxes
+ */
 
 namespace snowgoose {
-    
-template <class FT> struct Box {
-  Box(int dim = 0) : mA(dim), mB(dim)
-  {
-    mDim = dim;
-  }
 
- 
-  /**
-    * Box number of dimensions
-    */
-  int mDim;
+    template <class FT> struct Box {
 
-  /**
-   * "Left" box bounds
-   */
-  SmartArrayPtr<FT> mA;
+        Box(int dim = 0) {
+            mDim = dim;
+            mA = new FT[dim];
+            mB = new FT[dim];
+        }
 
-  /**
-   * "Right" box bounds
-   */
-  SmartArrayPtr<FT> mB;
-};
+        ~Box() {
+            delete[] mA;
+            delete[] mB;
+        }
+
+        Box(const Box& b) {
+            mDim = b.mDim;
+            mA = new FT[mDim];
+            mB = new FT[mDim];
+            std::memcpy(mA, b.mA, sizeof (FT) * mDim);
+            std::memcpy(mB, b.mB, sizeof (FT) * mDim);
+        }
+
+        Box(Box&& b) {
+            mA = b.mA;
+            mB = b.mB;
+            mDim = b.mDim;
+
+            b.mA = nullptr;
+            b.mB = nullptr;
+        }
+
+        /**
+         * Move assignment
+         * @param b
+         * @return 
+         */
+        Box& operator=(Box&& b) {
+            delete [] mA;
+            delete [] mB;
+            mA = b.mA;
+            mB = b.mB;
+            mDim = b.mDim;
+
+            b.mA = nullptr;
+            b.mB = nullptr;
+        }
+
+        /**
+         * Move assignment
+         * @param b
+         * @return 
+         *
+        Box& operator=(Box&& b) {
+            std::swap(mA, b.mA);
+            std::swap(mB, b.mB);
+            std::swap(mDim, b.mDim);
+        }*/
+
+        Box& operator=(const Box& b) = delete;
+
+        /**
+         * Box number of dimensions
+         */
+        int mDim;
+
+        /**
+         * "Left" box bounds
+         */
+        FT* mA;
+
+        /**
+         * "Right" box bounds
+         */
+        FT* mB;
+    };
+
 
 }
 
