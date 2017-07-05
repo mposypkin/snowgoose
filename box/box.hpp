180b78715c6317ee462a817ab5296524c234352e
Alexander Usov <alusov@mail.ru>
2017-07-05 13:03:21 +0300

    commit

--- box/box.hpp
+++ box/box.hpp
@@ -1,34 +1,62 @@
 #ifndef __BOX_HPP__
 #define __BOX_HPP__
 
-#include <common/smartptr.hpp>
 /**
   * Class for handling boxes
   */
 
+#include <string.h>
+
 namespace snowgoose {
     
 template <class FT> struct Box {
-  Box(int dim = 0) : mA(dim), mB(dim)
+  Box(int dim = 0)
   {
     mDim = dim;
+    mA = new FT[dim];
+    mB = new FT[dim];
   }
 
- 
+  ~Box(){
+      delete[] mA;
+      delete[] mB;
+  }
+
+  Box(const Box& b){
+      mDim = b.mDim;
+      mA = new FT[mDim];
+      mB = new FT[mDim];
+      memcpy(mA, b.mA, sizeof(FT) * mDim);
+      memcpy(mB, b.mB, sizeof(FT) * mDim);
+  }
+  
+  Box(Box&& b){
+      mA = b.mA;
+      mB = b.mB;
+      mDim = b.mDim;   
+      b.mA = nullptr;
+      b.mB = nullptr;
+      b.mDim = 0;
+  }
+
+ Box& operator=(Box&& b){return *this;};
+
+ Box& operator=(const Box& b) {return *this;}
+
   /**
     * Box number of dimensions
     */
   int mDim;
 
-  /**
+ /**
    * "Left" box bounds
    */
-  SmartArrayPtr<FT> mA;
+  FT* mA;
 
   /**
    * "Right" box bounds
    */
-  SmartArrayPtr<FT> mB;
+  FT* mB;
 };
 
 }
